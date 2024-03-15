279287bc91e52e3c18d7c6d5166030c1eff1e791
Alina7121 <113956800+Alina7121@users.noreply.github.com>
2024-03-15 10:49:24 -0400

    making adjustments on particles in style.css and index.html

---
+++ app.js
@@ -0,0 +1,8 @@
+    const typed = new Typed('.multiple-text', {
+   strings: ['Front-End Web Developer'],
+  typeSpeed: 250,
+ backSpeed: 50,
+    loop: true,
+   backDelay: () => Math.random() * 200 + 600 // Random delay between 200-600ms
+ });
+  
